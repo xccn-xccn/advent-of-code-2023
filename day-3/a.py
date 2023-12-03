ec713c249bb10de9d17a2275ddbf5d844de7b034
Jerry Min <jerryjmin7@gmail.com>
2023-12-03 20:34:18 +0000

    Day 3

--- day-3/a.py
+++ day-3/a.py
@@ -17,6 +17,41 @@ def get_input_file():
 
 def main():
     text = read_file(get_input_file())
+    specials = set() #char / number, (coords)
+    numbers = []
+    score = 0
+
+    for y, line in enumerate(text.splitlines()):
+        num = ["", []]
+        for x, char in enumerate(line):
+
+            if char.isdigit():
+                num[0] += char
+                num[1].append((y, x))
+            else:
+                if num[0]:
+                    numbers.append(num)
+                num = ["", []]
+
+            if not char.isalnum() and char != ".":
+                specials.add((y, x))
+        if num[0]:
+            numbers.append(num)
+    
+    final_specials = set()
+    for coord in specials:
+        for dy, dx in ((-1, -1), (0, -1), (-1, 0), (0, 0), (1, -1), (-1, 1), (1, 0), (0, 1), (1, 1)):
+            py, px = coord[0] + dy, coord[1] + dx
+
+            final_specials.add((py, px))
+
+    for num, coords in numbers:
+        if any(x in final_specials for x in coords):
+            score += int(num)
+
+    return score
+
+
 
 
 if __name__ == "__main__":
--- day-3/b.py
+++ day-3/b.py
@@ -17,6 +17,51 @@ def get_input_file():
 
 def main():
     text = read_file(get_input_file())
+    gears = [] #char / number, (coords)
+    numbers = []
+    score = 0
+
+    for y, line in enumerate(text.splitlines()):
+        num = ["", []]
+        for x, char in enumerate(line):
+
+            if char.isdigit():
+                num[0] += char
+                num[1].append((y, x))
+            else:
+                if num[0]:
+                    numbers.append(num)
+                num = ["", []]
+
+            if char == "*":
+                gears.append((y, x))
+        if num[0]:
+            numbers.append(num)
+    
+    # print(gears)
+    final_gears = []
+    for coord in gears:
+        final_gears.append(set())
+        for dy, dx in ((-1, -1), (0, -1), (-1, 0), (0, 0), (1, -1), (-1, 1), (1, 0), (0, 1), (1, 1)):
+            py, px = coord[0] + dy, coord[1] + dx
+
+            final_gears[-1].add((py, px))
+
+    # print(final_gears)
+    # print(numbers)
+    p_num = []
+    for g_coords in final_gears:
+        p_num = []
+        for num, n_coord in numbers:
+            if any(x in g_coords for x in n_coord):
+                p_num.append(int(num))
+
+                if len(p_num) == 2:
+                    score += p_num[0] * p_num[1]
+                    break
+    return score
+
+
 
 
 if __name__ == "__main__":
