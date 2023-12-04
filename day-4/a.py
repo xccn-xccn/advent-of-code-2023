083e92e281768892c47eede08af63a546f57eb84
Replit user <>
2023-12-04 13:31:46 +0000

    Day 4 complete

--- day-4/a.py
+++ day-4/a.py
@@ -2,6 +2,8 @@ from sys import argv
 from time import perf_counter
 
 start = perf_counter()
+
+
 def read_file(filename):
     with open(filename) as file:
         text = file.read()
@@ -17,6 +19,26 @@ def get_input_file():
 
 def main():
     text = read_file(get_input_file())
+    score = 0
+    for game in text.splitlines():
+        # print(game)
+        for i, char in enumerate(game):
+            if char == ":":
+                game = game[i + 1:]
+
+        win, have = game.split("|")
+        win, have = win.split(), have.split()
+        count = 0
+        # print(win, have)
+        for num in have:
+            if num in win:
+                # print(num, "in", win)
+                count += 1
+        # print(count)
+
+        if count:
+            score += 2**(count-1)
+    return score
 
 
 if __name__ == "__main__":
--- day-4/b.py
+++ day-4/b.py
@@ -2,6 +2,8 @@ from sys import argv
 from time import perf_counter
 
 start = perf_counter()
+
+
 def read_file(filename):
     with open(filename) as file:
         text = file.read()
@@ -16,9 +18,35 @@ def get_input_file():
 
 
 def main():
-    text = read_file(get_input_file())
+    text = read_file(get_input_file()).splitlines()
+
+
+    instances = [1 for _ in range(len(text))]
+    
+    for g, game in enumerate(text):
+        # print(game)
+        for i, char in enumerate(game):
+            if char == ":":
+                game = game[i + 1:]
+
+        card_count = instances[g]
+        win, have = game.split("|")
+        win, have = win.split(), have.split()
+        count = 0
+        # print(win, have)
+        for num in have:
+            if num in win:
+                # print(num, "in", win)
+                count += 1
+        # print(count)
+
+        for n in range(1, count + 1):
+            instances[g + n] += card_count
+    return sum(instances)
 
 
 if __name__ == "__main__":
     print(main())
     print(f'Time taken: {(perf_counter() - start) *1000} miliseconds')
+
+# 10212704
